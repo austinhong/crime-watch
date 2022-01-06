224e1f596de8b37adb0c7787dd23adae86f4b59e
austinhong <nistuagnoh@gmail.com>
2022-01-06 15:43:29 -0800

    Rename the app name space

--- CurrentPlaceDetailsOnMap/app/src/main/java/com/funland/mvcrime/MVCrime.java
+++ CurrentPlaceDetailsOnMap/app/src/main/java/com/funland/mvcrime/MVCrime.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.example.currentplacedetailsonmap;
+package com.funland.mvcrime;
 
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
